de1fd77a94a4bfc2fc250ed9d3e5877010a7ecd0
jay-rt <thapa.jayrt@gmail.com>
2024-03-09 12:22:01 -0500

    worked on home page and its styles and added some images to the project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </p>
+        <div className={styles.buttons}>
+          <button type="button" className={styles.button}>
+            Learn More
+          </button>
+          <button type="button" className={styles.button}>
+            Contact
+          </button>
+        </div>
+        <div className={styles.brand}>
+          <Image src="/brands.png" alt="" fill />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
