3d5784b5da742f015e4e75599e9ad5c5fa5b1f0f
jay-rt <thapa.jayrt@gmail.com>
2024-03-09 15:12:37 -0500

    customized the contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./contact.module.css";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.contact}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" className={styles.img} fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Sur name" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea rows={10} cols={30} placeholder="Message"></textarea>
+          <button type="button">Message</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
