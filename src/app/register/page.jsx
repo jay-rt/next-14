671ee691cb18ac163e7152d093a55c13abbbd47c
jay-rt <thapa.jayrt@gmail.com>
2024-05-27 08:28:13 -0400

    added registering functionality

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,10 +1,11 @@
+import { register } from "@/lib/actions";
 import styles from "./register.module.css";
 
 const RegisterPage = () => {
   return (
     <div className={styles.register}>
       <div className={styles.wrapper}>
-        <form className={styles.form}>
+        <form action={register} className={styles.form}>
           <input type="text" name="username" placeholder="username" />
           <input type="email" name="email" placeholder="email" />
           <input type="password" name="password" placeholder="password" />
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -3,6 +3,8 @@
 import Post from "@/models/Post";
 import connectToDB from "./utils";
 import { signIn, signOut } from "./auth";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
 
 export const addPost = async (formData) => {
   const { title, desc, slug, userId } = Object.fromEntries(formData);
@@ -39,4 +41,32 @@ export const handleGithubLogin = async () => {
 
 export const handleLogout = async () => {
   await signOut();
+};
+
+export const register = async (formData) => {
+  console.log(formData);
+  const { username, email, password, retypePassword } =
+    Object.fromEntries(formData);
+
+  if (password !== retypePassword) {
+    return "Password is incorrect";
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  try {
+    connectToDB();
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return "User already exists";
+    }
+
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    console.log("New user registered");
+  } catch (error) {
+    console.log(error);
+    return { error: "Something went wrong" };
+  }
 };
